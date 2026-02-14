687cfc425af05119cd4c24242f6c4c5de02f6d12
phamxuantoan1996 <phamxuantoandc@gmail.com>
2026-02-14 14:58:38 +0700

    write JobStepLift

--- MissionJob/include/mission_job.h
+++ MissionJob/include/mission_job.h
@@ -1,6 +1,8 @@
 #ifndef MISSION_JOB_H
 #define MISSION_JOB_H
 #include <string>
+#include <variant>
+#include <memory>
 enum class JobStatus
 {
     Init,
@@ -8,6 +10,27 @@ enum class JobStatus
     Finished,
     Failed
 };
+
+enum class DiscreteLevel {
+    LEVEL0,
+    LEVEL1,
+    LEVEL2,
+    LEVEL3,
+    LEVEL4,
+    LEVEL5,
+    LEVEL6,
+    LEVEL7,
+    LEVEL8,
+    LEVEL9
+};
+
+struct LiftLevelTarget {
+    DiscreteLevel level;
+};
+struct LiftHeightTarget {
+    float height;
+};
+using LiftTarget = std::variant<LiftLevelTarget,LiftHeightTarget>;
 
 class IJobStep {
     public:
@@ -37,13 +60,13 @@ class IJobStep {
 
             return status_;
         }
-    private:
+    protected:
         JobStatus status_{JobStatus::Init};
 };
 
-class MoveJobSRC : public IJobStep {
+class JobStepMovePath : public IJobStep {
     public:
-        MoveJobSRC(std::string target) : target_(target){
+        JobStepMovePath(std::string target) : target_(target){
             test_reached = 0;
         };
         void onEnter() override;
@@ -54,5 +77,52 @@ class MoveJobSRC : public IJobStep {
         int test_reached;
 };
 
+/*Lift Job*/
+class ILiftControl {
+    public:
+        void virtual moveTo(const LiftTarget& target) = 0;
+        bool virtual isReached() = 0;
+        bool virtual hasError() = 0;
+        void virtual stop() = 0;
+        virtual ~ILiftControl() = default;
+};
+class LiftControlLevel : public ILiftControl {
+    public:
+        void moveTo(const LiftTarget& target) override;
+        bool isReached() override;
+        bool hasError() override;
+        void stop() override; 
+};
+
+class LiftControlHeight : public ILiftControl {
+    public:
+        void moveTo(const LiftTarget& target) override;
+        bool isReached() override;
+        bool hasError() override;
+        void stop() override;
+};
+
+class JobStepLift : public IJobStep {
+    public:
+        JobStepLift(ILiftControl& lift,LiftTarget target):lift_(lift),target_(target){}
+        void onEnter() override;
+        void onExit() override;
+        JobStatus onTick() override;
+    private:
+        ILiftControl& lift_;
+        LiftTarget target_;
+        bool started_{false};
+};
+/***********************************/
+
+
+/*Fork Job*/
+class JobStepFork : public IJobStep {
+    public:
+        void onEnter() override;
+        void onExit() override;
+        JobStatus onTick() override;
+};
+/***********************************/
 
 #endif
--- MissionJob/src/mission_job.cpp
+++ MissionJob/src/mission_job.cpp
@@ -1,15 +1,17 @@
 #include "mission_job.h"
 #include <iostream>
-void MoveJobSRC::onEnter()
+void JobStepMovePath::onEnter()
 {
     std::cout << "Robot started moving\n";
 
-}
-void MoveJobSRC::onExit()
+};
+void JobStepMovePath::onExit()
 {
     std::cout << "Robot moved to " << target_ << std::endl;
-}
-JobStatus MoveJobSRC::onTick()
+};
+
+/****************Move Job****************/
+JobStatus JobStepMovePath::onTick()
 {
     // robot reached
     if(test_reached == 10)
@@ -18,5 +20,66 @@ JobStatus MoveJobSRC::onTick()
     }
     test_reached++;
     // 
+    return JobStatus::Running;
+};
+
+
+/*****************Lift Job***************/
+void LiftControlLevel::moveTo(const LiftTarget& target)
+{
+    auto levelTarget = std::get<LiftLevelTarget>(target);
+    std::cout << "Lift move to level : " << static_cast<int>(levelTarget.level) << std::endl;
+}
+bool LiftControlLevel::isReached()
+{
+    return true;
+}
+bool LiftControlLevel::hasError()
+{
+    return false;
+}
+void LiftControlLevel::stop()
+{
+    std::cout << "Lift stopped\n";
+}
+
+void LiftControlHeight::moveTo(const LiftTarget& target)
+{
+    auto levelTarget = std::get<LiftHeightTarget>(target);
+    std::cout << "Lift move to height : " << static_cast<int>(levelTarget.height) << std::endl;
+}
+bool LiftControlHeight::isReached()
+{
+    return true;
+}
+bool LiftControlHeight::hasError()
+{
+    return false;
+}
+void LiftControlHeight::stop()
+{
+    std::cout << "Lift stopped\n";
+}
+
+void JobStepLift::onEnter()
+{
+    if(started_)
+        return;
+    started_ = true;
+    lift_.moveTo(target_);
+    std::cout << "Job Lift Start\n";
+}
+void JobStepLift::onExit()
+{   
+    if(status_ == JobStatus::Running)
+        lift_.stop();
+    std::cout << "Job Lift Exit\n";
+}
+JobStatus JobStepLift::onTick()
+{
+    if(lift_.hasError())
+        return JobStatus::Failed;
+    if(lift_.isReached())
+        return JobStatus::Finished;
     return JobStatus::Running;
 }
